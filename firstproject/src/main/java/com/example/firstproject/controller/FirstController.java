004528e4bb37306356b0c2ac3b5d6e3e20589eef
taehwee_park <hweetae@gmail.com>
2023-10-06 14:30:25 +0900

    [New Feature] editing /hi page and create /bye page
    description:
    - model attribute
    - creating a bye web page

--- firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/FirstController.java
@@ -1,12 +1,19 @@
 package com.example.firstproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
     @GetMapping("/hi")
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+        model.addAttribute("username", "박휘");
         return "greetings";
+    }
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("username", "박태휘");
+        return "goodbye";
     }
 }
