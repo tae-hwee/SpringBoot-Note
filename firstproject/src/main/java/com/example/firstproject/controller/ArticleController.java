d1753b9f94fe8621f8ee8704c5f2ed2a1824f167
taehwee_park <hweetae@gmail.com>
2023-10-11 17:57:27 +0900

    [New Feature] Redirection
    description:
    - new article creation -> article get by id

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -34,7 +34,7 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
 //        System.out.println(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId();
     }
 
     @GetMapping("/articles/{id}")
