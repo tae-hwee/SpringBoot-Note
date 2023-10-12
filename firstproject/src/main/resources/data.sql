f06a7bbe50f4fcbbf89e8ceb436ed0c73c93e1cf
taehwee_park <hweetae@gmail.com>
2023-10-12 17:21:28 +0900

    [Config] dummy data insert

---
+++ firstproject/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO article (id, title, content) VALUES (1, 'First Title', 'First Content');
+INSERT INTO article (id, title, content) VALUES (2, 'Second Title', 'Second Content');
+INSERT INTO article (id, title, content) VALUES (3, 'Third Title', 'Third Content');
